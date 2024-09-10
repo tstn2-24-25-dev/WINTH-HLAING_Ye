8d7916abdbad4ee1fb337121e0dbbbfd4ceea064
yewinthlaing04 <yewinthlaing23@gmail.com>
2024-09-10 09:35:32 +0200

    first-commit

---
+++ HubertLaurentAlgo/exo1.js
@@ -0,0 +1,40 @@
+// create data set which contain ( a 1 ... f 4 )
+const dataset = ["a", 1, "b", 5, "c", 6, "d", 10, "e", 12, "f", 4];
+
+// score will associate to the word precedent ( like if a , score will be 1 , if f , score will be 4 )
+
+/**
+ * take the letter
+ * count the length of the letter
+ * match with the word in the dataset
+ * take the number precedent of the word
+ * sum the total
+ * and sort with the total
+ */
+const wordList = ["hubert", "basket"];
+
+const word = "hubert";
+
+const word1 = "lbal";
+
+for (list = 0; list < wordList.length; list++) {
+  for (w = 0; w < wordList[list].length; w++) {
+    //console.log(wordList[list].charAt(w));
+    for ( i = 0 ; i < dataset.length ; i++ ) {
+        
+    }
+  }
+}
+
+// for (w = 0; w < word1.length; w++) {
+//   for (i = 0; i < dataset.length; i++) {
+//     // console.log(word.charAt(w) + "" + dataset[i]);
+//     if (word1.charAt(w) == dataset[i]) {
+//       total += dataset[i + 1];
+//     }
+//   }
+// }
+
+// make the total score of letter
+
+// and sort from the biggest to the smallest
